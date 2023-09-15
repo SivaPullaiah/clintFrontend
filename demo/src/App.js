0ac9f65272aeb4381cad40181a6463b87a559858
siva <mangalisivapullaiah@gmail.com>
2023-09-15 15:50:46 +0530

    first commit

---
+++ demo/src/App.js
@@ -0,0 +1,61 @@
+
+import './App.css';
+import AboutUs from './component/AboutUs';
+import Home from './component/Home';
+import Navbar from './component/Navbar';
+import Servies from './component/Servies';
+import NoMatch from './component/NoMatch';
+import { Route,Routes } from 'react-router-dom';
+import Products from './component/Products';
+import New from './component/New';
+import Best from './component/Best';
+import Order from './component/Order';
+import User from './component/User';
+import UserDet from './component/UserDet';
+import Profile from './component/Profile';
+import Login from './component/Login';
+import { AuthProvider } from './component/Authentication';
+import App1 from './component/MainBody';
+import RequiredAuthentication from './component/RequiredAuthentication';
+import Signup from './component/Signup';
+import LoginSuccess from './component/LoginSuccess';
+import SignupSuccess from './component/SignupSuccess';
+import Code from './component/Code';
+// import Counter from './component/Counter';
+// import Click from './component/Click';
+// import Hover from './component/Hover';
+// import { UserProvider } from './component/ReactContext';
+// import A from './component/A';
+// import B from './component/B';
+function App() {
+  return (
+    <div className="App">
+      <AuthProvider>
+             <Navbar/>
+      <Routes>
+        <Route path="/" element={<App1/>}></Route>
+        <Route path="/aboutus" element={<AboutUs/>}></Route>
+        <Route path="*" element={<NoMatch/>}></Route>
+        <Route path='/order' element={<Order/>}></Route>
+        <Route path="/user" element={<User/>}>
+          <Route path=":userid" element={<UserDet/>}></Route>
+        </Route>
+        <Route path='/signup' element={<Signup/>}></Route>
+        <Route path='/profile' element={<RequiredAuthentication><Profile/></RequiredAuthentication>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/loginsuccess' element={<RequiredAuthentication><Code/></RequiredAuthentication>}></Route>
+        <Route path='/code' element={<Code/>}></Route>
+        <Route path='/signupsuccess' element={<SignupSuccess/>}></Route>
+      </Routes>
+      {/* <UserProvider value="Venu"><A/></UserProvider> */}
+      {/* <UserProvider value="20"><B/></UserProvider> */}
+      {/* <Counter render={(count,change)=> <Click count={count} change={change}/>}/>
+      <Counter render={(count,change)=> <Hover count={count} change={change}/>}/> */}
+      </AuthProvider>
+      
+    </div>
+    
+      );
+}
+
+export default App;
---
+++ demo/src/component/Welcome.js
@@ -0,0 +1,14 @@
+
+import React, { Component } from 'react'
+import './Welcome.css';
+export class Welcome extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className='demo' id='demo1'>Welcome to RGMCET</h1>
+      </div>
+    )
+  }
+}
+
+export default Welcome
